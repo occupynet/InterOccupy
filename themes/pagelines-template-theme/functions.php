bcc7f92066003a9f888432c734ddded2f42f9000
Drew <drew@nycga.net>
2012-07-12 18:54:08 -0400

    first(third) commit

---
+++ themes/pagelines-template-theme/functions.php
@@ -0,0 +1,30 @@
+<?php
+// Setup  -- Probably want to keep this stuff... 
+
+/**
+ * Hello and welcome to Base! First, lets load the PageLines core so we have access to the functions 
+ */	
+require_once( dirname(__FILE__) . '/setup.php' );
+	
+// For advanced customization tips & code see advanced file.
+	//--> require_once(STYLESHEETPATH . "/advanced.php");
+	
+// ====================================================
+// = YOUR FUNCTIONS - Where you should add your code  =
+// ====================================================
+
+
+// ABOUT HOOKS --------//
+	// Hooks are a way to easily add custom functions and content to PageLines. There are hooks placed strategically throughout the theme 
+	// so that you insert code and content with ease.
+
+
+// ABOUT FILTERS ----------//
+
+	// Filters allow data modification on-the-fly. Which means you can change something after it was read and compiled from the database,
+	// but before it is shown to your visitor. Or, you can modify something a visitor sent to your database, before it is actually written there.
+
+// FILTERS EXAMPLE ---------//
+
+	// The following filter will add the font  Ubuntu into the font array $thefoundry.
+	// This makes the font available to the framework and the user via the admin panel.
